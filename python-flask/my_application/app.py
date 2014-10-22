820074f8ea1d5624838a7a1186324a64f0025404
Armelle Gaha <d14123111@mydit.ie>
2014-10-22 14:05:30 +0000

    modification on app.py

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -12,5 +12,14 @@ def upload_file():
         f = request.files['file']
         f.save('./uploads/'+f.filename)
     return '',201
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+#show the post with the giving int id
+   return 'post %d' %post_id
+
+@app.route('/projects/')
+def project():
+   return "the project page"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
