ca033dcaa76556a1417fada4a5558ff2555b1ea5
Armelle Gaha <d14123111@mydit.ie>
2014-11-06 15:58:14 +0000

    add aws queues

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -1,6 +1,12 @@
 from flask import Flask
 from flask import request
 import os
+import boto.sqs
+import boto.sqs.queue
+from boto.sqs.message import Message
+from boto.sqs.connection import SQSConnection
+from boto.exception import SQSError
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -47,6 +53,32 @@ def euler_E2():
     if(factu%2 == 0):
       sumeven += factu
   result += str(sumeven) + "\n" 
+  return result
+
+
+@app.route('/listqueues')
+def queues():
+  result = ""
+  # This script created a queue
+  #
+  # Author - Paul Doyle Aug 2013
+  # modified by Gaha to list all queues in us-east-1 and eu-west-1
+  #
+
+
+  conn = boto.sqs.connect_to_region("us-east-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+
+  rs = conn.get_all_queues()
+  for q in rs:
+          result += q.id + "\n"
+
+  #irland is in eu-west-1 region
+  myconn = boto.sqs.connect_to_region("eu-west-1", aws_access_key_id='AKIAJ2BJXBF74JPNZKCQ', aws_secret_access_key='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')
+
+  rs = myconn.get_all_queues()
+  for q in rs:
+          result += q.id + "\n"
+
   return result
 
 @app.route('/post/<int:post_id>')
