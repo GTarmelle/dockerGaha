4e39b1a76b267ef09c271860ceb7c2c68ae43acb
Armelle Gaha <d14123111@mydit.ie>
2014-11-06 13:45:37 +0000

    returning a string imstead of an integer: euler1

--- python-flask/my_application/app.py
+++ python-flask/my_application/app.py
@@ -24,11 +24,13 @@ def show_files():
     return allFiles
 @app.route('/euler1')
 def euler_E1():
+    result = ""
     sum = 0
     for i in range(1000):
       if((i%3 == 0) or (i%5 == 0)):
         sum += i
-    return sum
+    result += str(sum) + "\n" 
+    return result
 
 @app.route('/post/<int:post_id>')
 def show_post(post_id):
